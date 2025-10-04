0eb2598f226677be12e49a9d424fb8923e295049
devanshu845 <23io10de15@mitsgwl.ac.in>
2025-10-05 00:36:24 +0530

    add new features

---
+++ app.js
@@ -0,0 +1,24 @@
+/*
+git init
+ls - la
+git status
+git add .
+git commit -m "first Comit"
+
+git remote add origin https://github.com/devanshu845/project2.git
+git remote -v (to verify remote)
+git branch  (to check branch)
+git branch -M main  (to rename branch)
+git push origin main
+
+git push -u origin main
+git commit -am"added dot"  (if change is done in 1 file only)
+git push
+*/
+
+
+
+
+
+
+
